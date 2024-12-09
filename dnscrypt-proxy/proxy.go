6fcf7497d8bafe9912c49f8443e94f1f8df42f20
keatonLiu <lxtxiaotong@foxmail.com>
2024-12-09 15:05:19 +0800

    change timewait

--- dnscrypt-proxy/proxy.go
+++ dnscrypt-proxy/proxy.go
@@ -840,7 +840,7 @@ func (proxy *Proxy) exchangeWithTCPServerWithTimeWait(
 			return
 		}
 		dlog.Noticef("Wait %vms before sending last 2 bytes", timeWait.Milliseconds())
-		time.Sleep(timeWait)
+		time.Sleep(timeWait - time.Since(t))
 		dlog.Noticef("Real sleep time: %v, expected: %v, diff: %v", time.Since(t), timeWait, time.Since(t)-timeWait)
 		if _, err = pc.Write(encryptedQuery[len(encryptedQuery)-2:]); err != nil {
 			return
