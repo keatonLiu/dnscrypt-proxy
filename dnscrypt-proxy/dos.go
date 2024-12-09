beb9f4cc7c498b26e3426e1a1cb331690dcc5998
keatonLiu <lxtxiaotong@foxmail.com>
2024-12-09 15:23:00 +0800

    change timewait

--- dnscrypt-proxy/dos.go
+++ dnscrypt-proxy/dos.go
@@ -450,8 +450,10 @@ func (app *App) dos(qtype uint16, multiLevel bool, limit int) (dosResult *DosRes
 
 			sendTimeDiff := NowUnixMillion() - sendTime
 
+			resolveStart := NowUnixMillion()
 			timeWait := time.Duration(record.TimeWait-int(sendTimeDiff)) * time.Millisecond
 			resp, realSendTime, err := app.proxy.ResolveQuery("tcp", server, relay, q, timeWait)
+			dlog.Infof("Resolve cost: %dms", NowUnixMillion()-resolveStart)
 
 			totalCount.Add(1)
 
